d376a6db966fbaf568cf3d071a304d0964436631
MiiZaaa <zavadilova.michaela1@gmail.com>
2022-02-22 18:25:25 +0100

    2 files modified

--- Ukoly/1.js
+++ Ukoly/1.js
@@ -1,1 +1,11 @@
 console.log ("Ahoj2");
+
+*/
+
+// 
+let jmeno = " Petra";
+let vek = 34;
+let vyska = 1.67;
+let vaha = 65;
+
+//Vzoreče
--- Ukoly/2.js
+++ Ukoly/2.js
@@ -1,1 +1,9 @@
+*/
 
+// vytvořím si proměnné
+let jmenoMuz = "Petr";
+let jmenoZena = "Jana";
+let rokNarozeniMuze = 1990;
+let rokNarozeniZeny = 1990;
+let mesicNarozeniMuze = 1;
+let mesicNarozeniZena = 7;
