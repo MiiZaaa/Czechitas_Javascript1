6b086901776b2b36d686cab73c60a0a5102701b2
MiiZaaa <zavadilova.michaela1@gmail.com>
2022-02-24 18:06:51 +0100

    1 file modified

---
+++ Ukoly/5.js
@@ -0,0 +1,13 @@
+// cyklus FOR
+// Ukol 1
+
+
+
+let dolni = Number(prompt("Dolni mez:"));
+let horni = Number(prompt("Horni mez:"));
+
+for (let i = dolni; i <= horni ; i = i++) {
+console.log(i);
+}
+
+
